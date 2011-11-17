3749ba1d3a33e860c8bed2721b08a1f5fbf62e45
Hursh <hsa470@gmail.com>
2011-11-17 15:16:31 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+group :development do
+	 gem "barista", "1.2.1" #coffeescript compilation
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forum::Application.config.secret_token = '92d96ef820d54f614d4a24b79ab3d10ace66a6bf0c14d381175e429be32ac91932a6f45a623f38f2088b8637afc3d8c6ac1f896f9d0373e391db9342e723dc63'
