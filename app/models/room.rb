7e614c20aca54ec146a55061f9631db93926b05a
Hursh <hsa470@gmail.com>
2011-11-17 17:09:20 -0500

    added scaffolds

---
+++ app/models/room.rb
@@ -0,0 +1,8 @@
+class Room < ActiveRecord::Base
+
+	belongs_to :user
+
+	has_many :posts
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Forum::Application.routes.draw do
+  resources :rooms
+
+  resources :posts
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
